218a5d6cf101aef43ceb748295f2d1e8f12eca43
BenStaite <ben.staite1999@hotmail.co.uk>
2021-02-10 03:35:50 +0000

    Initial Commit

---
+++ ActorCritic2.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 23:51:23 2021
+
+@author: Benst
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import StockEnv
+import time
+import pandas
+
+gamma = 0.99
+eps = np.finfo(np.float32).eps.item()
+
+num_batch = 5
+num_inputs = 5
+num_actions = 3
+num_hidden = 128
+
+inputs = layers.Input(shape=(num_batch,num_inputs))
+#common = layers.Dense(100, activation="relu")(inputs)
+lstm1 = layers.LSTM(50, return_sequences = True, activation="tanh")(inputs)
+drop1 = layers.Dropout(0.2)(lstm1)
+lstm2 = layers.LSTM(50, return_sequences = True, activation = 'relu')(drop1)
+drop2 = layers.Dropout(0.2)(lstm2)
+lstm3 = layers.LSTM(units = 25, return_sequences = False, activation = 'relu')(drop2)
+action = layers.Dense(num_actions, activation="softmax")(lstm3)
+critic = layers.Dense(1)(lstm3)
+
+model = keras.Model(inputs=inputs, outputs=[action, critic])
+
+optimizer = keras.optimizers.Adam(lr=0.01)
+huber_loss = keras.losses.Huber()
+action_probs_history = []
+critic_value_history = []
+rewards_history = []
+
+env = StockEnv.StockEnv("KO", 1000,num_batch, 0 )
+
+while True:  # Run until solved
+    state = env.States.to_numpy()
+    with tf.GradientTape() as tape: 
+        for timestep in range(1, 10):
+            x = tf.Variable([state], trainable=True, dtype=tf.float32)
+            # Predict action probabilities and estimated future rewards
+            # from environment state
+            action_probs, critic_value = model(x)
+            critic_value_history.append(critic_value[0, 0])
+
+            # Sample action from action probability distribution
+            action = np.random.choice(num_actions, p=np.squeeze(action_probs))
+            #print(action)
+            action_probs_history.append(tf.math.log(action_probs[0, action]))
+
+            # Apply the sampled action in our environment
+            state, reward = env.Step(action)
+            rewards_history.append(reward)
+            
+            template = "Balance: {:.3f}  Shares: {:.1f}  Price:{:.3f}  Action: {}  Reward: {:.3f}  Value:{:.3f}  Time: {}"
+            
+            print(template.format(env.State.Balance * env.BalScale, env.State.Shares * env.ShareScale, 
+                                  env.State.Price* env.PriceScale, env.LastAction, reward*env.RewardScale, 
+                                  env.GetStateValue(env.State), env.TimeStep))
+
+
+        # Calculating loss values to update our network
+        history = zip(action_probs_history, critic_value_history, rewards_history)
+        actor_losses = []
+        critic_losses = []
+        for log_prob, value, ret in history:
+            # At this point in history, the critic estimated that we would get a
+            # total reward = `value` in the future. We took an action with log probability
+            # of `log_prob` and ended up recieving a total reward = `ret`.
+            # The actor must be updated so that it predicts an action that leads to
+            # high rewards (compared to critic's estimate) with high probability.
+            diff = ret - value
+            actor_losses.append(-log_prob * diff)  # actor loss
+
+            # The critic must be updated so that it predicts a better estimate of
+            # the future rewards.
+            critic_losses.append(
+                huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+            )
+
+        # Backpropagation
+        loss_value = sum(actor_losses) + sum(critic_losses)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        # Clear the loss and reward history
+        action_probs_history.clear()
+        critic_value_history.clear()
+        rewards_history.clear()
+
+
+
+
+
+
+
---
+++ StockEnv.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 00:36:36 2021
+
+@author: Benst
+"""
+import pandas
+import numpy as np
+from pandas_datareader import data as pdr
+import yfinance as yf
+import copy
+
+class State:
+    def __init__(self, date, price, volume, shares, balance):
+        self.Date = date
+        self.Price = price
+        self.Volume = volume
+        self.Shares = shares
+        self.Balance = balance
+    
+    def to_array(self):
+        array = []
+        array.append(self.Date)
+        array.append(self.Price)
+        array.append(self.Volume)
+        array.append(self.Shares)
+        array.append(self.Balance)
+       # print(array)
+        return array
+    
+    
+class StockEnv:
+    
+    def __init__(self, ticker, startBal, batchsize, timestep):
+        data = self.LoadData(ticker)
+        self.BatchSize = batchsize
+        self.Finished = False
+        self.Data, self.PriceScale, self.VolumeScale = self.FormatData(data)
+        self.TimeStep = timestep
+        self.BalScale = 2000
+        self.RewardScale = 1
+        self.ShareScale = 100
+        firstrow = self.Data.iloc[timestep]
+        self.State = State(firstrow['Datetime'], firstrow['Price'], firstrow['Volume'], 0, startBal/self.BalScale)
+        self.States =pandas.DataFrame(np.zeros((self.BatchSize,5)))
+        self.States.iloc[0] = self.State.to_array()
+        
+    def LoadData(self, ticker):
+        yf.pdr_override() # <== that's all it takes :-)
+        ftse = yf.Ticker(ticker)
+        #data = ftse.history(period= "1wk", interval="1m")
+        data = ftse.history(start="2021-01-29", end="2021-02-03", interval = "1m")
+        data2 = ftse.history(start="2021-01-22", end="2021-01-29", interval = "1m")
+        data3 = ftse.history(start="2021-01-15", end="2021-01-22", interval = "1m")
+        #data4 = ftse.history(start="2021-01-10", end="2021-01-15", interval = "1m")
+        #data5 = ftse.history(start="2021-01-01", end="2021-01-08", interval = "1m")
+        data = pandas.concat([data,data2,data3], axis = 0)
+        data = data.reset_index()
+        return data
+    
+    def FormatData(self, data):    
+        Price = data['Open']
+        Volume = data['Volume']
+        Time = (data['Datetime'].dt.day) + (data['Datetime'].dt.hour/24) + (data['Datetime'].dt.minute/1440)
+        Time = Time/3000
+        X = pandas.DataFrame(Time)
+        X ['Price'] = Price/Price.max()
+        
+        X['Volume'] = Volume/Volume.max()
+        return X, Price.max(), Volume.max()
+    
+    def Step(self, action):
+        self.Penalty = 0
+        self.PreviousState = copy.deepcopy(self.State)
+        self.States = self.States.shift(1)
+       # self.AllStates.append(self.State.to_array())
+        if (action == 0):
+            self.Hold()
+        elif(action == 1):
+            self.Buy()
+        else:
+            self.Sell()
+        self.State.Shares = (round(self.State.Shares*self.ShareScale))/self.ShareScale
+        reward = self.GetReward()
+        self.States.iloc[0] = self.State.to_array()
+        if(self.TimeStep > len(self.Data.index)-4):
+            self.Finished = True
+        return self.States.to_numpy(), reward
+    
+    def GetStateValue(self, state):
+        return ((state.Balance * self.BalScale) + ((state.Shares*self.ShareScale) * (state.Price* self.PriceScale)))
+    
+    def GetReward(self):
+        thisR = self.GetStateValue(self.State)
+        lastR = self.GetStateValue(self.PreviousState)
+        return (thisR - lastR)/self.RewardScale
+    
+    def AdvanceTime(self):
+        self.TimeStep+=1
+        row = self.Data.iloc[self.TimeStep]
+        self.State.Date = row['Datetime']
+        self.State.Price = row['Price']
+        self.State.Volume = row['Volume']
+    
+    def Buy(self):
+        self.LastAction = "Buy"
+        if((self.State.Balance * self.BalScale) - (self.State.Price * self.PriceScale) < 0):
+            self.Penalty = 1
+            self.Hold()
+        else:
+            self.State.Balance -= (self.State.Price * self.PriceScale)/self.BalScale
+            self.State.Shares += (1/ self.ShareScale)
+            self.AdvanceTime()
+    
+    def Hold(self):
+        self.LastAction = "Hold"
+        self.AdvanceTime()
+        
+    def Sell(self):
+        self.LastAction = "Sell"
+        if(self.State.Shares * self.ShareScale < 1):
+            self.Hold()
+            self.Penalty = 1
+        else:
+            self.State.Balance += (self.State.Price * self.PriceScale)/self.BalScale
+            self.State.Shares -= (1/ self.ShareScale)
+            self.AdvanceTime()
+    
+            
